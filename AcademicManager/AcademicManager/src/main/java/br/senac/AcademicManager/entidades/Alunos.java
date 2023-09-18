1831c63275aab9f5e636e1b0acc5602a78511f4a
Leonardo Certorio <118929833+LeonardoCertorio@users.noreply.github.com>
2023-09-18 13:07:07 -0300

    Update Alunos.java

--- AcademicManager/AcademicManager/src/main/java/br/senac/AcademicManager/entidades/Alunos.java
+++ AcademicManager/AcademicManager/src/main/java/br/senac/AcademicManager/entidades/Alunos.java
@@ -1,8 +1,6 @@
 package br.senac.AcademicManager.entidades;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
 
 @Entity
 public class Alunos {
