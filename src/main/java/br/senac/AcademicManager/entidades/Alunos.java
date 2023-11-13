9025320194095941d5bfa10655675853187e7960
Leonardo Certorio <118929833+LeonardoCertorio@users.noreply.github.com>
2023-11-13 20:16:56 -0300

    Update Alunos.java

--- src/main/java/br/senac/AcademicManager/entidades/Alunos.java
+++ src/main/java/br/senac/AcademicManager/entidades/Alunos.java
@@ -1,19 +1,15 @@
 package br.senac.AcademicManager.entidades;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Alunos {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
     @Column(nullable = false, length = 15)
     private String primeiroNome;
     @Column(nullable = false, length = 20)
@@ -27,66 +23,4 @@ public class Alunos {
     private String semestre;
     @Column(nullable = false, length = 15)
     private String status;
-
-    public String getPrimeiroNome() {
-        return primeiroNome;
-    }
-
-    public void setPrimeiroNome(String primeiroNome) {
-        this.primeiroNome = primeiroNome;
-    }
-
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
-
-
-    public String getSemestre() {
-        return semestre;
-    }
-
-    public void setSemestre(String semestre) {
-        this.semestre = semestre;
-    }
-
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
