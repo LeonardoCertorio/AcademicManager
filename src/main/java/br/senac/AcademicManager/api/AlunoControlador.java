e51f00ac8eb58ed1c8f16cf272bf139928f3c996
Leonardo Certorio <118929833+LeonardoCertorio@users.noreply.github.com>
2023-11-13 20:26:44 -0300

    add api files

---
+++ src/main/java/br/senac/AcademicManager/api/AlunoControlador.java
@@ -0,0 +1,54 @@
+package br.senac.AcademicManager.alunos.api;
+
+import br.senac.AcademicManager.alunos.dominio.Aluno;
+import br.senac.AcademicManager.alunos.dominio.AlunoRepositorio;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/geral/alunos")
+@AllArgsConstructor
+public class AlunoControlador {
+
+    private AlunoRepositorio alunoRepositorio;
+
+    @GetMapping
+    public List<Aluno> findAll(){
+        return alunoRepositorio.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Aluno> findById(@PathVariable long id){
+        return alunoRepositorio.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Aluno> create(@RequestBody Aluno aluno){
+        return ResponseEntity.ok((alunoRepositorio.save(aluno)));
+    }
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Long id) {
+        alunoRepositorio.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Aluno> update(@PathVariable("id") long id,
+                                 @RequestBody Aluno aluno) {
+        return alunoRepositorio.findById(id)
+                .map(record -> {
+                    record.setNome(aluno.getNome());
+                    record.setEmail(aluno.getEmail());
+                    record.setDepartamento(aluno.getDepartamento());
+                    record.setDataNascimento(aluno.getDataNascimento());
+                    record.setTelefone(aluno.getTelefone());
+                    Aluno updated = alunoRepositorio.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+}
